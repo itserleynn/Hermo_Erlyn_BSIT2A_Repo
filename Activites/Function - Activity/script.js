a5b9976bdb983f9347e937852227567680c63071
Your Name <you@example.com>
2025-03-24 16:29:01 +0800

    Activities\Function - Activity

---
+++ Activites/Function - Activity/script.js
@@ -0,0 +1,15 @@
+function whatShallIWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    } else {
+        console.log("Wear a sweater")
+    }
+}
+
+
+whatShallIWear(50);
+whatShallIWear(80);
+whatShallIWear(60);
+
---
+++ Getting Functional/function.js
@@ -0,0 +1,40 @@
+var dogName = "Kitty";
+var  dogweight = 14;
+if (dogweight > 20) {
+    console.log(kitty + " says, WOOF WOOF")
+} else {
+    console.log(kitty + " says WOOF WOOF")
+}
+
+    
+var dogName = "weinz";
+var  dogweight = 14;
+if (dogweight > 20) {
+    console.log(weinz + " says, WOOF WOOF")
+} else {
+    console.log(weinz + " says WOOF WOOF")
+}
+
+    
+var dogName = "tef";
+var  dogweight = 14;
+if (dogweight > 20) {
+    console.log(tef + " says, WOOF WOOF")
+} else {
+    console.log(tef + " says WOOF WOOF")
+}
+
+    
+var dogName = "tey";
+var  dogweight = 14;
+if (dogweight > 20) {
+    console.log(tey+ " says, WOOF WOOF")
+} else {
+    console.log(tey + " says WOOF WOOF")
+}
+
+    
+bark("kitty", 35);
+bark("weinz", 19);
+bark("tef", 40);
+bark("tey", 26);
