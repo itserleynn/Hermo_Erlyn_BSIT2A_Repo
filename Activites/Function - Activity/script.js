49cc54d2b4d3003a64239adaedc427982d6c077c
itserleynn <earlynhermo@gmail.com>
2025-03-27 17:01:22 +0800

    Activity 3

--- Activites/Function - Activity/script.js
+++ Activites/Function - Activity/script.js
@@ -1,15 +1,37 @@
-function whatShallIWear(temp) {
-    if (temp < 60) {
-        console.log("Wear a jacket");
-    } else if (temp < 70) {
-        console.log("Wear a t-shirt");
+// // function whatShallIWear(temp) {
+// //     if (temp < 60) {
+// //         console.log("Wear a jacket");
+// //     } else if (temp < 70) {
+// //         console.log("Wear a t-shirt");
+// //     } else {
+// //         console.log("Wear a sweater")
+// //     }
+// // }
+
+
+// // whatShallIWear(50);
+// // whatShallIWear(60);
+// // whatShallIWear(80);
+
+function calculateArea(r) {
+    var area;
+    if (r <= 0) {
+        return 0;
     } else {
-        console.log("Wear a sweater")
+        area = Math.PI * r * r;
+        return area;
     }
 }
 
+var radius = 5.2;
 
-whatShallIWear(50);
-whatShallIWear(60);
-whatShallIWear(80);
+var theArea = calculateArea(radius);
 
+console.log("The area is: " + theArea)
+
+// function printName(firstName, lastName) {
+//     var fullNamed = firstName + ' ' + lastName;
+//     return fullNamed
+// }
+
+// console.log('My full name is ' + printName('Vel', 'Pretty'))
